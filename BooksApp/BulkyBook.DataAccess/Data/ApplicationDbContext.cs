4946849653e3ae7734cc21bfd8b293293fe86cc0
Keroles-Elsaid <keroelsaid15@gmail.com>
2024-03-19 18:41:17 +0200

    Ecommerce-Books-App

---
+++ BooksApp/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,151 @@
+﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<Company> Companies { get; set; }
+		public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+		public DbSet<OrderHeader> OrderHeaders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Company>().HasData(
+				new Company {
+                    Id = 1,
+                    Name = "Drills and More", 
+					StreetAddress = "34 East", 
+					City = "Charleston", 
+					State = "South Carolina", 
+					PostalCode = "45887",
+					PhoneNumber = "555-555-5456"
+				},
+                new Company
+                {
+                    Id = 2,
+                    Name = "Party Organizers",
+                    StreetAddress = "42 Oak Street",
+                    City = "Aiken",
+                    State = "South Carolina",
+                    PostalCode = "45717",
+                    PhoneNumber = "555-666-5326"
+                },
+                new Company
+                {
+                    Id = 3,
+                    Name = "Healthy Foods",
+                    StreetAddress = "11 Second St",
+                    City = "Spartanburg",
+                    State = "South Carolina",
+                    PostalCode = "32456",
+                    PhoneNumber = "333-532-5431"
+                }
+            );
+
+			modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+		
+            modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Id = 1,
+					Title = "Fortune of Time",
+					Author = "Billy Spark",
+					Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+					ISBN = "SWD9999001",
+					ListPrice = 99,
+					Price = 90,
+					Price50 = 85,
+					Price100 = 80,
+					CategoryId = 1,
+					ImageUrl = ""
+				},
+				new Product
+				{
+					Id = 2,
+					Title = "Dark Skies",
+					Author = "Nancy Hoover",
+					Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+					ISBN = "CAW777777701",
+					ListPrice = 40,
+					Price = 30,
+					Price50 = 25,
+					Price100 = 20,
+					CategoryId = 3,
+					ImageUrl = ""
+				},
+				new Product
+				{
+					Id = 3,
+					Title = "Vanish in the Sunset",
+					Author = "Julian Button",
+					Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+					ISBN = "RITO5555501",
+					ListPrice = 55,
+					Price = 50,
+					Price50 = 40,
+					Price100 = 35,
+					CategoryId = 1,
+					ImageUrl = ""
+				},
+				new Product
+				{
+					Id = 4,
+					Title = "Cotton Candy",
+					Author = "Abby Muscles",
+					Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+					ISBN = "WS3333333301",
+					ListPrice = 70,
+					Price = 65,
+					Price50 = 60,
+					Price100 = 55,
+					CategoryId = 3,
+					ImageUrl = ""
+				},
+				new Product
+				{
+					Id = 5,
+					Title = "Rock in the Ocean",
+					Author = "Ron Parker",
+					Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+					ISBN = "SOTJ1111111101",
+					ListPrice = 30,
+					Price = 27,
+					Price50 = 25,
+					Price100 = 20,
+					CategoryId = 3,
+					ImageUrl = ""
+				},
+				new Product
+				{
+					Id = 6,
+					Title = "Leaves and Wonders",
+					Author = "Laura Phantom",
+					Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+					ISBN = "FOT000000001",
+					ListPrice = 25,
+					Price = 23,
+					Price50 = 22,
+					Price100 = 20,
+					CategoryId = 2,
+					ImageUrl = ""
+				}
+				);
+		}
+	}
+}
